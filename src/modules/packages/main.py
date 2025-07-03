c61ed5402dd7efd138b8594d7c6a85b43ff16415
AshlynOrSomethin <31773733+AshlynOrSomethin@users.noreply.github.com>
2025-07-02 20:51:45 -0500

    try generic calamares packages module

--- src/modules/packages/main.py
+++ src/modules/packages/main.py
@@ -514,12 +514,6 @@ class PMPacman(PackageManager):
         self.run_pacman(["pacman", "-Rs", "--noconfirm"] + pkgs, True)
 
     def update_db(self):
-        try:
-            check_target_env_call(["ping", "-c", "1", "google.com"])
-            libcalamares.utils.debug("Internet connectivity confirmed in chroot.")
-        except subprocess.CalledProcessError:
-            libcalamares.utils.warning("No internet in chroot environment.")
-            raise RuntimeError("No network access in target environment.")
         self.run_pacman(["pacman", "-Sy"])
 
     def update_system(self):
